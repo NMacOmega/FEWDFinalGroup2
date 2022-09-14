1defc15faf36cedd91a17239a33c7ac9e99e55aa
NMacOmega <nathaniel.maciver@gmail.com>
2022-09-13 22:06:23 -0400

    Built site structure and created common nav/footer with handlebars integration - Nathaniel 9/13/2022

---
+++ js/partials.js
@@ -0,0 +1,46 @@
+let navPartial = `
+    <ul class= 'nav__list'>
+        <li class = 'nav__item--logo'>
+            <a class = 'nav__link--logo' href = './index.html'><span class="nav__logo">Group 2</span></a>
+        </li>
+        <li class = 'nav__item'>
+            <a class = 'nav__link' href = './mission.html'><span class="nav__text">Our Mission</span></a>
+        </li>
+        <li class = 'nav__item'>
+            <a class = 'nav__link' href = './questionnaire.html'><span class="nav__text">Questionnaire</span></a>
+        </li>
+        <li class = 'nav__item'>
+            <a class = 'nav__link' href = './prompt.html'><span class="nav__text">Prompt</span></a>
+        </li>   
+    </ul>`;
+let footerPartial = `
+<ul class= 'footer__list'>
+    <li class = 'footer__item'>
+        <a class = 'footer__link' href = './index.html'><span class="footer__text">Home</span></a>
+    </li>
+    <li class = 'footer__item'>
+        <a class = 'footer__link' href = './mission.html'><span class="footer__text">Our Mission</span></a>
+    </li>
+    <li class = 'footer__item'>
+        <a class = 'footer__link' href = './questionnaire.html'><span class="footer__text">Questionnaire</span></a>
+    </li>
+    <li class = 'footer__item'>
+        <a class = 'footer__link' href = './prompt.html'><span class="footer__text">Prompt</span></a>
+    </li>   
+</ul>
+<p class="footer__copyright">&copy; 2022 Group 2 Studios</p>
+`;
+
+let headerTemplate = Handlebars.compile(navPartial);
+let footerTemplate = Handlebars.compile(footerPartial);
+
+let nav = document.createElement("nav");
+let footer = document.createElement("footer");
+
+nav.innerHTML = navPartial;
+nav.classList.add("nav");
+footer.innerHTML = footerPartial;
+footer.classList.add("footer");
+
+document.querySelector("#nav").replaceWith(nav);
+document.querySelector("#footer").replaceWith(footer);
